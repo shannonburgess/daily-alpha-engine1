b6dbd5819809e225645356d987d5dc316016bb4a
shannonburgess <shannon.burgess@gmail.com>
2026-08-16 16:22:47 -0700

    Fix Pine processor import lint

--- src/daily_alpha/pine_processor.py
+++ src/daily_alpha/pine_processor.py
@@ -9,9 +9,10 @@ from __future__ import annotations
 
 import json
 import os
+from collections.abc import Mapping
 from dataclasses import asdict, dataclass
 from datetime import UTC, datetime
-from typing import Any, Mapping
+from typing import Any
 
 from .signals import PineSignal, SignalAction, SignalError, parse_pine_signal
 
